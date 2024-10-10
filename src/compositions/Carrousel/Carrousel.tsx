7ae411c29ff8dab63de697e99fd131ca78aa1563
skinnynoizze <skinnynoizze@protonmail.com>
2024-10-10 07:32:25 +0200

    Banner height 300px and link

--- src/compositions/Carrousel/Carrousel.tsx
+++ src/compositions/Carrousel/Carrousel.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import Link from 'next/link'
 import styles from './Carrousel.module.scss'
 
 
@@ -7,6 +8,7 @@ interface SlideData {
   alt: string
   title: string
   description: string
+  link: string
 }
 
 const Carrousel: React.FC = () => {
@@ -15,28 +17,39 @@ const Carrousel: React.FC = () => {
 
   const slides: SlideData[] = [
     {
-      image: '/images/carrousel/carrousel01.png',
+      image: '/images/carrousel/usa-elections.jpg',
+      alt: '',
+      title: 'Who Will Rule the Ice?',
+      description: 'Glide into the prediction market and let your political picks soar!',
+      link: '/politics/international-tournaments/2024-presidential-election-usa/1001000000001596805632',
+    },
+    {
+      image: '/images/carrousel/carrousel05.png',
       alt: '',
       title: 'Freeze Your Competition!',
       description: 'Our odds are so good, they\'ll give you chills!',
+      link: '',
     },
     {
       image: '/images/carrousel/carrousel02.png',
       alt: '',
       title: 'Cool Deals to Chill Your Wallet',
       description: 'Waddle your way to crypto riches with PinWin!',
+      link: '',
     },
     {
       image: '/images/carrousel/carrousel03.png',
       alt: '',
       title: 'Ice, Ice, Baby... It\'s Winning Time!',
       description: 'Slide into victory with our frosty selection of games',
+      link: '',
     },
     {
       image: '/images/carrousel/carrousel04.png',
       alt: '',
       title: 'Experience the Thrill of Winning',
       description: 'Where winning is just the tip of the iceberg!',
+      link: '',
     },
   ]
 
@@ -47,7 +60,7 @@ const Carrousel: React.FC = () => {
         setCurrentSlide((prevSlide) => (prevSlide + 1) % slides.length)
         setIsTransitioning(false)
       }, 500) // Half of the transition duration
-    }, 5000) // Change slide every 5 seconds
+    }, 10000) // Change slide every 5 seconds
 
     return () => clearInterval(interval)
   }, [ slides.length ])
@@ -61,10 +74,12 @@ const Carrousel: React.FC = () => {
             className={`${styles.bannerSlide} ${index === currentSlide ? styles.active : ''}`}
             style={{ backgroundImage: `url(${slide.image})` }}
           >
-            <div className={`${styles.slideContent} ${isTransitioning ? styles.transitioning : ''}`}>
-              <h2 className={styles.slideTitle}>{slide.title}</h2>
-              <p className={styles.slideDescription}>{slide.description}</p>
-            </div>
+            <Link href={slide.link} className={styles.slideLink}>
+              <div className={`${styles.slideContent} ${isTransitioning ? styles.transitioning : ''}`}>
+                <h2 className={styles.slideTitle}>{slide.title}</h2>
+                <p className={styles.slideDescription}>{slide.description}</p>
+              </div>
+            </Link>
           </div>
         ))
       }
