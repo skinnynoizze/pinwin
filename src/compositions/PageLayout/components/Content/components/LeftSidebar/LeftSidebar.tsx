75bef63fa3da44b1288ea112a76d489518c43eab
skinnynoizze <skinnynoizze@protonmail.com>
2024-10-21 18:29:19 +0200

    added gitbook icon

--- src/compositions/PageLayout/components/Content/components/LeftSidebar/LeftSidebar.tsx
+++ src/compositions/PageLayout/components/Content/components/LeftSidebar/LeftSidebar.tsx
@@ -20,6 +20,9 @@ const LeftSidebar: React.FC = () => {
           <a href="https://discord.com/invite/pinwinxyz" target="_blank" rel="noopener noreferrer">
             <Icon icon="akar-icons:discord-fill" className="text-xl hover:text-brand-50 transition-colors" />
           </a>
+          <a href="https://pinwin-xyz.gitbook.io/pinwin.xyz-docs" target="_blank" rel="noopener noreferrer">
+            <Icon icon="simple-icons:gitbook" className="text-xl hover:text-brand-50 transition-colors" />
+          </a>
         </div>
       </div>
       <div className="overflow-auto flex-grow no-scrollbar bg-bg-l1 border border-grey-10 rounded-md px-2 py-2">
--- src/compositions/PageLayout/components/Footer/components/Navbar/Navbar.tsx
+++ src/compositions/PageLayout/components/Footer/components/Navbar/Navbar.tsx
@@ -5,14 +5,6 @@ import { constants } from 'helpers'
 
 import messages from './messages'
 
-// Array for text links
-const textItems = [
-  {
-    text: messages.nav.docs,
-    link: 'https://pinwin-xyz.gitbook.io/pinwin.xyz-docs',
-    icon: 'simple-icons:gitbook',
-  },
-]
 
 // Array for social icons
 const socialItems = [
@@ -23,6 +15,10 @@ const socialItems = [
   {
     link: 'https://discord.com/invite/your_invite_code', // Replace with your actual Discord invite link
     icon: 'akar-icons:discord-fill', // Discord icon
+  },
+  {
+    link: 'https://pinwin-xyz.gitbook.io/pinwin.xyz-docs',
+    icon: 'simple-icons:gitbook',
   },
 ]
 
@@ -41,22 +37,6 @@ const Navbar: React.FC = () => {
                 target="_blank"
               >
                 <Icon icon={item.icon} className="text-grey-60 hover:text-grey-90 text-xl" />
-              </a>
-            ))
-          }
-        </div>
-        <div className="flex items-center space-x-3">
-          {
-            textItems.map((item, index) => (
-              <a
-                key={`${item.link}-${index}`}
-                className="flex items-center space-x-1"
-                href={item.link}
-                rel="noreferrer"
-                target="_blank"
-              >
-                <Icon icon={item.icon} className="text-grey-60 text-lg" />
-                <Message value={item.text} className="font-medium text-grey-60 hover:text-grey-90 hover:underline" />
               </a>
             ))
           }
